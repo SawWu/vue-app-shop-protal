60a265ebe49febb5e151cd77aa47b38114c6f45e
Liu Ren <liuren@cc1st.com>
2018-08-11 20:44:39 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store';
+import {Button, Row, Col, Swipe, SwipeItem} from 'vant'
 
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem)
 Vue.config.productionTip = false;
 
 new Vue({
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="wrapper">
-    <h1>Hello Vue</h1>
+  <div>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3"><img :src="locationIcon" width="80%" class="location-icon"/></van-col>
+        <van-col span="16">
+          <input type="text" class="search-input"/>
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini" style="margin: 0 0 0 0.5rem">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <div class="swiper-area">
+      <van-swipe :autoplay="3000">
+        <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+          <img :src="banner.imageUrl" width="100%"/>
+        </van-swipe-item>
+      </van-swipe>
+    </div>
   </div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex';
+
+  import location from '../assets/images/location.png'
+  import swiper1 from '../assets/images/1.jpg'
+  import swiper2 from '../assets/images/2.jpg'
+  import swiper3 from '../assets/images/3.jpg'
 
   export default {
     name: 'Home',
@@ -14,14 +35,42 @@
     created() {
 
     },
-    data: () => ({}),
+    data: () => ({
+      locationIcon: location,
+      bannerPicArray: [
+        {imageUrl: swiper1},
+        {imageUrl: swiper2},
+        {imageUrl: swiper3},
+      ],
+    }),
     methods: {}
   }
 </script>
 
 <style lang="scss" scoped>
-  .wrapper {
+  .search-bar {
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+    overflow: hidden;
+  }
+
+  .search-input {
+    width: 100%;
+    height: 1.3rem;
+    border-bottom: 1px solid #fff !important;
+    background-color: #e5017d;
+    color: #fff;
+  }
+
+  .location-icon {
+    padding-top: .2rem;
+    padding-left: .4rem;
+  }
+
+  .swiper-area {
     width: 20rem;
+    clear: both;
   }
 
 </style>
